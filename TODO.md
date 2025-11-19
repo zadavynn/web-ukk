37123d14c7912b99e48d91053d0b2e9c1ff5b6f9
Davine <zadavynn@gmail.com>
2025-11-19 10:31:33 +0700

    memperbaiki route

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
-# TODO: Center Table Content in Admin Index Views
+# TODO: Update Kegiatan Terbaru Table in Admin Dashboard
 
--   [x] Update resources/views/admin/sponsor/index.blade.php
--   [x] Update resources/views/admin/panitia/index.blade.php
--   [x] Update resources/views/admin/kegiatan/index.blade.php
--   [x] Update resources/views/admin/catatan/index.blade.php
--   [x] Update resources/views/admin/absensi/index.blade.php
+## Tasks
+
+-   [x] Modify the query in AdminController.php to prioritize unfinished kegiatans (status != 'selesai') first, then by latest created_at, while limiting to 3 records.
+-   [x] Verify the changes work as expected (no specific test command, assume manual verification).
--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,33 @@ Route::get('/admin', [AdminController::class, 'index'])->name('admin');
 Route::get('/admin/logout', [AdminController::class, 'logout'])->name('logout');
 
 // halaman absensi admin
-Route::resource('absensi', AbsensiController::class);
+Route::get('absensi', [AbsensiController::class, 'index']);
+Route::post('absensi', [AbsensiController::class, 'store']);
+Route::put('absensi/{id}', [AbsensiController::class, 'update']);
+Route::delete('absensi/{id}', [AbsensiController::class, 'destroy']);
 Route::get('/admin/absensi/get-available-panitia/{kegiatanId}', [AbsensiController::class, 'getAvailablePanitia'])->name('absensi.available-panitia');
 
 // halaman catatan admin
-Route::resource('catatan', CatatanController::class);
+Route::get('catatan', [CatatanController::class, 'index']);
+Route::post('catatan', [CatatanController::class, 'store']);
+Route::put('catatan/{id}', [CatatanController::class, 'update']);
+Route::delete('catatan/{id}', [CatatanController::class, 'destroy']);
 
 // halaman kegiatan admin
-Route::resource('kegiatan', KegiatanController::class);
+Route::get('kegiatan', [KegiatanController::class, 'index']);
+Route::post('kegiatan', [KegiatanController::class, 'store']);
+Route::put('kegiatan/{id}', [KegiatanController::class, 'update']);
+Route::delete('kegiatan/{id}', [KegiatanController::class, 'destroy']);
 Route::post('/kegiatan/{id}/finish', [KegiatanController::class, 'finish'])->name('kegiatan.finish');
 
 // halaman panitia admin
-Route::resource('panitia', PanitiaController::class);
+Route::get('panitia', [PanitiaController::class, 'index']);
+Route::post('panitia', [PanitiaController::class, 'store']);
+Route::put('panitia/{id}', [PanitiaController::class, 'update']);
+Route::delete('panitia/{id}', [PanitiaController::class, 'destroy']);
 
 // halaman sponsor admin
-Route::resource('sponsor', SponsorController::class);
+Route::get('sponsor', [SponsorController::class, 'index']);
+Route::post('sponsor', [SponsorController::class, 'store']);
+Route::put('sponsor/{id}', [SponsorController::class, 'update']);
+Route::delete('sponsor/{id}', [SponsorController::class, 'destroy']);
